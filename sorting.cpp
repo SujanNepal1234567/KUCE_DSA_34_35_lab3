3516999f6e46cd6aa39d5df712fef3526f3b3138
kabichneu <kabichneu@gmail.com>
2020-02-27 10:31:56 +0545

    implemented algorithm of heapsort into a program.. debugging left

---
+++ sorting.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+void Heapify(int a[],int n,int i){
+	int l,r,largest,temp=0;
+	largest = i;
+	l=2*i+1;
+	r = 2*i+2;
+	if(l<n && a[i]<a[l]){
+		largest = l;
+	}
+	if(r<n && a[largest]<a[r]){
+		largest = r;
+	}
+	if(largest!=i){
+		swap(a[i], a[largest]); 
+
+	}
+}
+
+void build_heap(int a[]){
+	int n,i;
+	n = 10;
+	for(i=n-1;i>=0;i--){
+		Heapify(a,n,i);
+	}
+}
+
+void HeapSort(int a[]){
+	int i,n,temp;
+	n = 10;
+	build_heap(a);
+	for(i=n-1;i>=0;i--){
+		swap(a[i], a[0]); 
+		Heapify(a,n,0);
+		
+	}
+}
+
+int main(){
+	int a[10]={4,3,2,5,8,7,9,1,6,10};
+	
+	cout<<"Before sorting::"<<endl;
+	for(int i = 0 ; i<10;i++){
+		cout<<a[i];
+		cout<<endl;
+	}
+	HeapSort(a);
+	
+	cout<<"After sorting::"<<endl;
+	for(int i = 0 ; i<10;i++){
+		cout<<a[i];
+		cout<<endl;
+	}
+	return 0;
+}
+
